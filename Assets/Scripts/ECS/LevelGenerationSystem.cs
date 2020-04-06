56e3e0060e682a91eedfd03865806440f3124e3f
chrigi28 <christian.holinger@gmail.com>
2020-04-06 17:32:23 +0200

    new dots approch

---
+++ Assets/Scripts/ECS/LevelGenerationSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+using Random = Unity.Mathematics.Random;
+
+public class LevelGenerationSystem : ComponentSystem
+{
+    private Random random;
+    private bool generated;
+
+    protected override void OnCreate()
+    {
+        random = new Random();
+    }
+
+    protected override void OnUpdate()
+    {
+        if (!this.generated)
+        {
+            Entity cubeEntity = EntityManager.Instantiate(PrefabEntities.PrefabEntity);
+            EntityManager.SetComponentData(cubeEntity,
+                new Translation
+                    {Value = new float3(random.NextFloat(-5, 5), random.NextFloat(2), random.NextFloat(-5, 5))});
+            EntityManager.SetComponentData(cubeEntity, new Rotation {Value = random.NextQuaternionRotation()});
+            this.generated = true;
+        }
+    }
+}
