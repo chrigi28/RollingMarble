e778578075e1a5c492131952b66009b5e470a369
chrigi28 <christian.holinger@gmail.com>
2020-04-08 16:09:02 +0200

    dots only scene

---
+++ Assets/Scripts/ECS/Systems/PlayerInputSystem.cs
@@ -0,0 +1,13 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+
+namespace Assets.Scripts.ECS.Systems
+{
+    public class PlayerInputSystem : JobComponentSystem
+    {
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        {
+            
+        }
+    }
+}
